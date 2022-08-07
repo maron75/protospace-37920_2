91f82ea60c627e3fc1eecd41d4251f2b7aa994e1
maron75 <hirokana.827562113@gmail.com>
2022-08-07 14:41:18 +0900

    修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :prototypes
-  has_many :comments      
-
-  validates :password_confirmation,presence: true
+         
   validates :name, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
-  validates :position, presence: true    
+  validates :position, presence: true
+       
+  has_many :prototypes
+  has_many :comments
+       
 end
